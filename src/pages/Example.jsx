95744639f85647472faf967669018f0262463e19
techstackmedia1 <osagienoah04@gmail.com>
2022-10-02 08:45:46 -0700

    implement useMemo hook

---
+++ src/pages/Example.jsx
@@ -0,0 +1,17 @@
+import UseMemo from "../Hooks/UseMemo";
+import UseRef1 from "../Hooks/Useref1";
+import Useref2 from "../Hooks/Useref2";
+import Useref3 from "../Hooks/Useref3";
+
+const Example = () => {
+  return (
+    <>
+      <UseRef1 />
+      <Useref2 />
+      <Useref3 />
+      <UseMemo />
+    </>
+  );
+};
+
+export default Example;
