cc6cccb8593e97cf002f417aa638b0056e67c1aa
techstackmedia1 <osagienoah04@gmail.com>
2022-10-02 18:44:10 -0700

    commit changes - useRef hook

--- src/pages/Example.jsx
+++ src/pages/Example.jsx
@@ -1,5 +1,3 @@
-import UseCallback from "../Hooks/UseCallback";
-import UseMemo from "../Hooks/UseMemo";
 import UseRef1 from "../Hooks/Useref1";
 import Useref2 from "../Hooks/Useref2";
 import Useref3 from "../Hooks/Useref3";
@@ -7,11 +5,9 @@ import Useref3 from "../Hooks/Useref3";
 const Example = () => {
   return (
     <>
-      {/* <UseRef1 />
+      <UseRef1 />
       <Useref2 />
       <Useref3 />
-      <UseMemo /> */}
-      <UseCallback />
     </>
   );
 };
